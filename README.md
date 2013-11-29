ef869314372ab7d6fb7ac56c68d964fadb654385
Sina Sharafzadeh <sina.sharafzadeh@gmail.com>
2013-11-29 04:37:57 +0330

    dev 0.1

---
+++ README.md
@@ -0,0 +1,19 @@
+#Under Development
+
+## Web-based collaboration tool
+This is a collaboration  tool suitable for TA classes , teaching classes , your office collaboration , any where you have connected audience and you don't want to use projector or 
+the board , your clients have to interact with your workflow recurcivly and move their 
+head up and down from the board to monitor .
+
+
+## Features
+your clients just need a browser!
+desktop sharing support throw browser .
+online text editor with collaboration support .
+online slide sharing .
+switching all above tools like a charm .
+
+## System Requirements
+nodejs
+vnc server
+
---
+++ app.js
@@ -0,0 +1,62 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+function getIPAddresses() {
+
+    var ipAddresses = [];
+
+    var interfaces = require('os').networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                ipAddresses.push(alias.address);
+            }
+        }
+    }
+
+    return ipAddresses;
+}
+console.log(getIPAddresses());
+
+var io = require('socket.io').listen(8080);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('myClick', function (data) {
+        socket.broadcast.emit('myClick', data);
+    });
+});
